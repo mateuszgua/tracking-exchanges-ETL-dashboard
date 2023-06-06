87565e635e3be90fd982a7c1619c1109ece73568
Mateusz Gula <mateuszgua@wp.eu>
2023-06-06 13:44:50 +0200

    Feat: add function for load data to database, add functions in main file for load data

--- app/main.py
+++ app/main.py
@@ -1,25 +1,36 @@
 from get_pandas_df import get_pandas_df
 from config import Config
 from database_manager import DatabaseManager
-
+from database_load_data import FillTables
+from database_reader import DatabaseReader
 db_manager = DatabaseManager()
 
 tables_names = ['indexData', 'indexInfo', 'indexProcessed']
 
-try:
-    file_path = Config.FILE_PATH_INDEX_DATA
-    df_index_data = get_pandas_df(file_path)
-
-    file_path = Config.FILE_PATH_INDEX_INFO
-    df_index_info = get_pandas_df(file_path)
-
-    file_path = Config.FILE_PATH_INDEX_PROCESSED
-    df_index_processed = get_pandas_df(file_path)
+try:            
 
     db_manager.is_database_exist()
 
     for table_name in tables_names:
         db_manager.is_table_exist(table_name)
+    
+    db_reader = DatabaseReader()
+    
+    for table_name in tables_names:
+        table_is_empty = db_reader.is_empty(table_name)
         
+        if table_is_empty is True:
+            load_data = FillTables()
+            match table_name:
+                case "indexData":
+                    file_path = Config.FILE_PATH_INDEX_DATA
+                case "indexInfo":
+                    file_path = Config.FILE_PATH_INDEX_INFO
+                case "indexProcessed":
+                    file_path = Config.FILE_PATH_INDEX_PROCESSED
+            dataframe = get_pandas_df(file_path)
+            load_data.fill_table(table_name, dataframe)
+        else:
+                print(f"Table: {table_name} have data")
 except Exception as e:
         print(f"Error in main program with: {e}")
