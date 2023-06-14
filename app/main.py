5cda410a6e0cf2de21d28f099438c0270db3c40f
Mateusz Gula <mateuszgua@wp.eu>
2023-06-14 21:46:46 +0200

    Feat: add function for get df from database and print data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+import pandas as pd
+import pyodbc
+
 from get_pandas_df import get_pandas_df
 from config import Config
 from database_manager import DatabaseManager
 from database_load_data import FillTables
 from database_reader import DatabaseReader
+
+
 db_manager = DatabaseManager()
 
 tables_names = ['indexData', 'indexInfo', 'indexProcessed']
@@ -33,4 +38,26 @@ try:
         else:
                 print(f"Table: {table_name} have data")
 except Exception as e:
-        print(f"Error in main program with: {e}")
+    print(f"Error in main program with: {e}")
+
+def get_dataframe_from_sql(sql_file_path):
+    try:
+        # db_manager = MyDatabase()
+        sql_file = open(sql_file_path)
+        sql_as_string = sql_file.read()
+        result = db_reader.sql_get_all(sql_as_string)
+        result_df = pd.DataFrame(result)
+    except pyodbc.Error as e:
+        print(e)
+        return f"Error: {e}"
+    finally:
+        sql_file.close()
+        return result_df
+    
+try:
+    sql_file_task1 = "app/sql_files/task_1.sql"
+    df_task1 = get_dataframe_from_sql(sql_file_task1)
+    print(df_task1)
+except pyodbc.Error as e:
+    print(e)
+
